c63cb046ea2143a6cff88826ccbef9dae9f4ce87
Constantine Theocharis <cthe@mailbox.org>
2023-10-06 15:15:00 +0000

    Add FS dependency

--- deps.ts
+++ deps.ts
@@ -1,3 +1,4 @@
 export * as path from "https://deno.land/std@0.197.0/path/mod.ts";
+export * as fs from "https://deno.land/std@0.197.0/fs/mod.ts";
 export * as io from "https://deno.land/std@0.197.0/io/mod.ts";
 export * as cliffy from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
