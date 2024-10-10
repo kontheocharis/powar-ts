7e98731c0ff7729bb850c2a83ac1d68a6cf5888d
Orhan Yigit Yazicilar <yigityazicilar@gmail.com>
2024-10-10 18:59:14 +0100

    Update powar_init.ts
    Co-authored-by: Constantine Theocharis <cthe@mailbox.org>

--- powar_init.ts
+++ powar_init.ts
@@ -14,7 +14,7 @@ async function init() {
       "The path to initialise the project in.",
       { required: true }
     )
-    .action(async (args: { path: string; }) => {
+    .action(async (args) => {
       const log = makeLogger();
 
       // Copy the example folder to the given path.
